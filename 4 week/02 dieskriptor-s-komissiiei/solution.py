bb1b383d6ec56e1222b9798f230fd5db7542600d
George <glomukhin@gmail.com>
2022-03-05 23:09:31 +0700

    fix misprint

---
+++ 4 week/02 dieskriptor-s-komissiiei/solution.py
@@ -0,0 +1,13 @@
+class Value():
+
+    def __init__(self):
+        self.value = None
+        
+    def __get__(self, instance, owner):
+        return self.value
+    
+    def __set__(self, instance, value):
+        self.value = (1 - instance.commission) * value
+
+
+
